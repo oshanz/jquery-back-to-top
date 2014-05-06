23ecb95ff58bc2e0adba51692742127d44fa5709
Oshanz <spiritualmirror@gmail.com>
2014-05-06 19:15:27 +0530

    minified

--- back2top.js
+++ back2top.js
@@ -1,19 +1,32 @@
 (function($) {
-    $.fn.back2top = function() {
+    $.fn.back2top = function(limit, top, speed) {
+        var l = limit || 500, s = speed || 0, t = top || 0;
         var b2t = $('#back2top');
         if (!b2t.length) {
             $("body").append("<div id='back2top'></div>");
             b2t = $('#back2top');
+            b2t.css({
+                "display": "none",
+                "background-image": "url('http://localhost/jquery-back-to-top/demo/back2top.png')",
+                "background-repeat": "no-repeat",
+                "cursor": "pointer",
+                "position": "fixed",
+                "bottom": "20px",
+                "height": "70px",
+                "right": "20px",
+                "width": "70px",
+                "z-index": "1",
+            });
         }
         $(window).scroll(function() {
-            if ($(this).scrollTop() > 800) {
+            if ($(this).scrollTop() > l) {
                 b2t.fadeIn();
             } else {
                 b2t.fadeOut();
             }
         });
         b2t.click(function() {
-            $("body,html").animate({scrollTop: 0}, 200);
+            $("body,html").animate({scrollTop: t}, s);
         });
     };
 })(jQuery);

d5b9014d31ba5701c307c2750cef457675110de9
Oshanz <spiritualmirror@gmail.com>
2014-05-06 16:24:04 +0530

    cool

--- jquery.b2t.js
+++ jquery.b2t.js
@@ -1,46 +1,19 @@
-(function($, window, document) {
-    var pluginName = 'backtotop',
-            defaults = {
-                backToTopTxt: "back to top"
-            };
-
-    function Plugin(element, options) {
-        this.element = element;
-        this.options = $.extend({}, defaults, options);
-        this._defaults = defaults;
-        this._name = pluginName;
-        this.init();
-    }
-
-    Plugin.prototype.init = function() {
-        $backToTopEle = $(this.element);
-        $backToTopEle.text(this.options.backToTopTxt)
-                .attr("title", this.options.backToTopTxt)
-                .click(function() {
-                    $("html, body").animate({scrollTop: 0}, 120);
-                }),
-                $backToTopFun = function() {
-                    var st = $(document).scrollTop(),
-                            winh = $(window).height();
-                    (st > 0) ? $backToTopEle.show() : $backToTopEle.hide();
-                    //IE6 positioning
-                    if (!window.XMLHttpRequest) {
-                        $backToTopEle.css("top", st + winh - 166);
-                    }
-                };
-        $(window).bind("scroll", $backToTopFun);
-        $(function() {
-            $backToTopFun();
-        });
-    };
-
-    $.fn[pluginName] = function(options) {
-        return this.each(function() {
-            if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName,
-                        new Plugin(this, options));
+(function($) {
+    $.fn.b2t = function() {
+        var mari = $('#mari');
+        if (!mari.length) {
+            $("body").append("<div id='mari'></div>");
+            mari = $('#mari');
+        }
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 800) {
+                mari.fadeIn();
+            } else {
+                mari.fadeOut();
             }
         });
+        mari.click(function() {
+            $("body,html").animate({scrollTop: 0}, 200)
+        });
     };
-
-})(jQuery, window, document);
+})(jQuery);
